6a4fdbf52743f33144c8250ec5f3efcd6304e089
WhiteW00lf <mocha_cookie_crumble@proton.me>
2026-02-03 17:55:57 +0530

    fixed date

--- dags/daily_dags.py
+++ dags/daily_dags.py
@@ -12,11 +12,13 @@ BUCKET_NAME = 'apple-data-raw'
 load_dotenv()
 key = os.getenv('API_KEY')
 
-def fetch_raw_data(execution_date, **kwargs):
-    date_str = execution_date.strftime('%Y-%m-%d')
-    end_date = (execution_date + timedelta(days=1)).strftime('%Y-%m-%d')
-    raw_file = f"raw/date={date_str}/aapl.csv"
-    client = storage.Client()
+def fetch_raw_data_task(execution_date, **kwargs):
+    print("🔥 FUNCTION STARTED 🔥")
+
+    end_date = execution_date.strftime('%Y-%m-%d')
+    start_date = (execution_date - timedelta(days=1)).strftime('%Y-%m-%d')
+    raw_file = f"raw/date={start_date}/aapl.csv"
+    client = storage.Client('market-data-analytics-486307')
     bucket = client.bucket(BUCKET_NAME)
     blob = bucket.blob(raw_file)
     
@@ -25,10 +27,15 @@ def fetch_raw_data(execution_date, **kwargs):
         raise AirflowSkipException(f"File {raw_file} already exists in bucket {BUCKET_NAME}")
 
 
-    url = f"https://api.twelvedata.com/time_series?apikey={key}&interval=1day&start_date={date_str}&end_date={end_date}&format=CSV&type=stock&exchange=NASDAQ&country=US&symbol=AAPL&timezone=exchange"
+    url = f"https://api.twelvedata.com/time_series?apikey={key}&interval=1day&start_date={start_date}&end_date={end_date}&format=CSV&type=stock&exchange=NASDAQ&country=US&symbol=AAPL&timezone=exchange"
+    print("Calling TwelveData API:", url)
 
-    response = requests.get(url)
-    print(response.status_code)
+    response = requests.get(url,timeout=30)
+    response.raise_for_status()
+
+    print("Response received, length:", len(response.text))
+
+
     if response.status_code == 200:
         blob.upload_from_string(response.text,content_type='text/csv')
         print(f"File {raw_file} uploaded to bucket {BUCKET_NAME}")
@@ -57,7 +64,7 @@ with DAG(
 
     fetch_raw_data = PythonOperator(
     task_id='fetch_raw_data',
-    python_callable=fetch_raw_data,
+    python_callable=fetch_raw_data_task,
     dag=dag
 )
 
